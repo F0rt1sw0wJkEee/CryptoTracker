661845ceb7a7ba74c765939cd793027456dd15dc
Hudyma Serhii <111682159+F0rt1sw0wJkEee@users.noreply.github.com>
2023-12-05 08:45:35 +0200

    update file

---
+++ UWP/ViewModels/HomeViewModel.cs
@@ -0,0 +1,18 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Messaging;
+using System;
+using System.Collections.ObjectModel;
+using UWP.Models;
+
+namespace UWP.ViewModels {
+    public partial class HomeViewModel : ObservableRecipient {
+
+		[ObservableProperty]
+		private ObservableCollection<HomeCard> priceCards = new ObservableCollection<HomeCard>();
+		
+		public void InAppNotification(string title, string message = "") {
+			var tuple = new Tuple<string, string>(title, message);
+			Messenger.Send(new NotificationMessage(tuple));
+		}
+	}
+}
