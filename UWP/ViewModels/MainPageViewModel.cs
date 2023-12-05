523bc34106eb81b05fcb85104bf8c9a0e332e47a
Hudyma Serhii <111682159+F0rt1sw0wJkEee@users.noreply.github.com>
2023-12-05 08:45:35 +0200

    update file

---
+++ UWP/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,41 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Messaging;
+using CommunityToolkit.Mvvm.Messaging.Messages;
+using System;
+
+namespace UWP.ViewModels {
+    public partial class MainPageViewModel : ObservableRecipient {
+
+        public MainPageViewModel() {
+            IsActive = true;
+        }
+
+        protected override void OnActivated() {
+            Messenger.Register<MainPageViewModel, NotificationMessage>(this, (r, m) => {
+                InfoBarTitle = m.Value.Item1;
+                InfoBarMessage = m.Value.Item2;
+            });
+        }
+
+        // ####################################################################
+        private string infoBarMessage = "";
+        public string InfoBarMessage {
+            get => infoBarMessage;
+            set {
+                SetProperty(ref infoBarMessage, value);
+                InfoBarOpened = true;
+            }
+        }
+
+        [ObservableProperty]
+        private string infoBarTitle = "";
+
+        [ObservableProperty]
+        private bool infoBarOpened = false;
+    }
+
+    public sealed class NotificationMessage : ValueChangedMessage<Tuple<string, string>> {
+        public NotificationMessage(Tuple<string, string> tuple) : base(tuple) {
+        }
+    }
+}
