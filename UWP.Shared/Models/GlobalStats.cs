bc37a9e54dbdd27114cb7a6a3f727f69241ee3da
Hudyma Serhii <111682159+F0rt1sw0wJkEee@users.noreply.github.com>
2023-12-05 08:45:53 +0200

    update file

---
+++ UWP.Shared/Models/GlobalStats.cs
@@ -0,0 +1,8 @@
+namespace UWP.Models {
+	public class GlobalStats {
+        public double TotalMarketCap { get; set; } = 0;
+        public double TotalVolume { get; set; } = 0;
+        public double BtcDominance { get; set; } = 0;
+        public string CurrencySymbol { get; set; }
+    }
+}
