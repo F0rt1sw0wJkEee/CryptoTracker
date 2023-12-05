fd3ee104db469123d7e77da45dd3d8af7cebee2a
Hudyma Serhii <111682159+F0rt1sw0wJkEee@users.noreply.github.com>
2023-12-05 08:45:46 +0200

    update file

---
+++ UWP.Shared/Constants/Currencies.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+
+namespace UWP.Shared.Constants {
+    public class Currencies {
+        public static string GetCurrencySymbol(string currency) {
+            string symbol;
+            return CurrencySymbol.TryGetValue(currency ?? "", out symbol) ? symbol : " ";
+        }
+
+        /// TODO: Once I add a good way to convert currencies, add the following:
+        /// { "PHP", "₱"},
+        /// { "ZAR", "R"},
+
+        public static readonly Dictionary<string, string> CurrencySymbol = new Dictionary<string, string>() {
+            { "ARS", "$" },
+            { "AUD", "$" },
+            { "BRL", "R$"},
+            { "BTC", "₿"},
+            { "CAD", "$" },
+            { "CNY", "¥" },
+            { "CZK", "Kč" },
+            { "ETH", "Ξ" },
+            { "EUR", "€" },
+            { "GBP", "£" },
+            { "ILS", "₪" },
+            { "INR", "₹" },
+            { "JPY", "¥" },
+            { "MXN", "$" },
+            { "PLN", "zł"},
+            { "USD", "$" },
+        };
+    }
+}
