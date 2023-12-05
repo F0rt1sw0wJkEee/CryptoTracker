07e125a1f5cb8cf4fad45de7698e10379fbd0c39
Hudyma Serhii <111682159+F0rt1sw0wJkEee@users.noreply.github.com>
2023-12-05 08:45:33 +0200

    update file

---
+++ UWP/ViewModels/CoinCompactViewModel.cs
@@ -0,0 +1,18 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using UWP.Models;
+
+namespace UWP.ViewModels {
+    partial class CoinCompactViewModel : ObservableObject {
+
+		[ObservableProperty]
+		private Coin info = new Coin();
+
+		
+		[ObservableProperty]
+		private ChartModel chart = new ChartModel();
+
+
+		[ObservableProperty]
+		private CoinDetailsViewModel coinDetailsVM;
+	}
+}
