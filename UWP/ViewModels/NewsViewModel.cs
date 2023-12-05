c20987d36d1b0c07d199ae3e7b8aa547e8f7a6bd
Hudyma Serhii <111682159+F0rt1sw0wJkEee@users.noreply.github.com>
2023-12-05 08:45:35 +0200

    update file

---
+++ UWP/ViewModels/NewsViewModel.cs
@@ -0,0 +1,16 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using UWP.Models;
+
+namespace UWP.ViewModels {
+    partial class NewsViewModel : ObservableRecipient {
+
+        [ObservableProperty]
+        private List<NewsData> news = Enumerable.Repeat(new NewsData(), 30).ToList();
+
+
+        public List<NewsCategories> Categories { get; set; }
+        public List<string> Filters { get; set; } = new List<string>();
+    }
+}
