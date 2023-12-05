121f90d407bd430f3e1e9b1a644259242bf87d98
Hudyma Serhii <111682159+F0rt1sw0wJkEee@users.noreply.github.com>
2023-12-05 08:45:25 +0200

    update file

---
+++ UWP/UserControls/ChartAreaPiP.xaml.cs
@@ -0,0 +1,25 @@
+using UWP.Models;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace UWP.UserControls {
+    public sealed partial class ChartAreaPiP : UserControl {
+        public ChartAreaPiP() {
+            this.InitializeComponent();
+        }
+
+        public static readonly DependencyProperty ChartModelProperty =
+        DependencyProperty.Register(
+            nameof(ChartModel),
+            typeof(ChartModel),
+            typeof(ChartAreaPiP),
+            null);
+
+        public ChartModel ChartModel {
+            get => (ChartModel)GetValue(ChartModelProperty);
+            set => SetValue(ChartModelProperty, value);
+        }
+    }
+}
