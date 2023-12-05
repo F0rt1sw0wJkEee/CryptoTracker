5274a56d6247b2a74bc92d1880760cc69beb6dbb
Hudyma Serhii <111682159+F0rt1sw0wJkEee@users.noreply.github.com>
2023-12-05 08:45:55 +0200

    update file

---
+++ UWP.Shared/Models/HomeCard.cs
@@ -0,0 +1,11 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace UWP.Models {
+	public partial class HomeCard : ObservableObject {
+		[ObservableProperty]
+		private Coin info = new Coin();
+
+		[ObservableProperty]
+		private ChartModel chart = new ChartModel();
+	}
+}
